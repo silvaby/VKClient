ea589ccdcfe5452f653ffb7c7f3afe8542b959da
Dzmitry <skysilvaby@gmail.com>
2020-10-28 23:57:10 +0300

    Update

--- VKClient/Controller/SettingsViewController.swift
+++ VKClient/Controller/SettingsViewController.swift
@@ -11,10 +11,5 @@ import UIKit
 class SettingsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-
-//        AF.request("https://api.vk.com/method/newsfeed.get").responseJSON { response in
-//            print(response)
-//        }
-//        print("viewDidLoad ended")
     }
 }
