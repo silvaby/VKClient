fb50eb681eede57ccc5d3573258549a580eba0c3
Dzmitry <skysilvaby@gmail.com>
2020-10-28 19:13:34 +0300

    Add project

---
+++ VKClient/Application/AppDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.swift
+//  VKClient
+//
+//  Created by Dzmitry on 10/28/20.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+}
+
---
+++ VKClient/Application/SceneDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  SceneDelegate.swift
+//  VKClient
+//
+//  Created by Dzmitry on 10/28/20.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+}
+
