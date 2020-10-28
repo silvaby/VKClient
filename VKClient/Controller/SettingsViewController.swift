2b497b0b8a5a8264bc3fa5748ea9594f503b1edc
Dzmitry <skysilvaby@gmail.com>
2020-10-28 19:45:44 +0300

    Add SettingsView

---
+++ VKClient/Controller/SettingsViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SettingsViewController.swift
+//  VKClient
+//
+//  Created by Dzmitry on 10/28/20.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
