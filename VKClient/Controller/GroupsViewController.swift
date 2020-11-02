55b353ec8d4a751c862b44914d399898ebd92456
Dzmitry <skysilvaby@gmail.com>
2020-11-02 23:50:48 +0300

    Add groups

---
+++ VKClient/Controller/GroupsViewController.swift
@@ -0,0 +1,12 @@
+//
+//  GroupsViewController.swift
+//  VKClient
+//
+//  Created by Dzmitry on 11/2/20.
+//
+
+import UIKit
+
+class GroupsViewController: UIViewController {
+    override func viewDidLoad() {}
+}
